fece8cef5c13b9bbb8848c53742dadb15e57d9eb
AnnemarieMcDonnelltemp <36330527+AnnemarieMcDonnelltemp@users.noreply.github.com>
2019-12-24 05:42:07 +0000

    Add files via upload

---
+++ sql tables.sql
@@ -0,0 +1,19 @@
+/create table
+
+CREATE TABLE book(
+				id int NOT NULL AUTO INCREMENT,
+				title varchar(250),
+				author varchar(250),
+				price int,
+				PRIMARY KEY(ID)
+				);
+				
+
+DESC TABLE =VIEWS COLUMN AND CONTENTS OF SAME.
+
+SELECT * FROM book= give output of all the books in te current dadabase.
+
+// POPULATE TABLE.
+
+INSERT INTO book(title, author, price) values ('Christmas', 'Jamie Oliver', 20);
+
