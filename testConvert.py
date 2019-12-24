df7e9c4ce8d29c29d118b001e6f9e8553889bc13
AnnemarieMcDonnelltemp <36330527+AnnemarieMcDonnelltemp@users.noreply.github.com>
2019-12-24 05:20:06 +0000

    Delete testConvert.py

--- testConvert.py
+++
@@ -1,9 +0,0 @@
-colnames=['id','Title','Author','Price']
-result=(1,"mary", "story", 21)
-item = {}
-
-for i, colName in enumerate(colnames):
-    value = result[i]
-    item[colName] = value
-
-print(item)
